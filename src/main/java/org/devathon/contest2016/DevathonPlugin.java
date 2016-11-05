ec90b607dd441923029853de9957972647aa29f1
Keith Mitchell <ldd2000@live.ca>
2016-11-05 10:01:10 -0700

    Added sign detection for creating machines

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -1,12 +1,28 @@
 package org.devathon.contest2016;
 
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DevathonPlugin extends JavaPlugin {
 
+    public static DevathonPlugin INSTANCE;
+
+    public String SignHead = "[machine]";
+    public Material MachineBlock = Material.IRON_BLOCK;
+
     @Override
     public void onEnable() {
         // put your enable code here
+        INSTANCE = this;
+
+        this.getConfig().addDefault("sign.head", "[machine]");
+        this.getConfig().addDefault("machines.block", Material.IRON_BLOCK.toString());
+        this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
+
+        SignHead = this.getConfig().getString("sign.head");
+        MachineBlock = Material.valueOf(this.getConfig().getString("machines.block").toUpperCase());
+
         getServer().getPluginManager().registerEvents(new Events(), this);
     }
 
--- src/main/java/org/devathon/contest2016/Events.java
+++ src/main/java/org/devathon/contest2016/Events.java
@@ -2,10 +2,15 @@ package org.devathon.contest2016;
 
 import java.util.*;
 
+import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.material.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
@@ -17,13 +22,14 @@ public class Events implements Listener {
         GivePlayerHelpBook(event.getPlayer());
     }
 
+
     public void GivePlayerHelpBook(Player player){
         ItemStack book = new ItemStack(Material.WRITTEN_BOOK, 1);
 
         BookMeta meta = (BookMeta)book.getItemMeta();
 
         meta.setAuthor("Server");
-        meta.setTitle("Machine FAQ");
+        meta.setTitle("Machine FAQ"); // < 16 chars
 
         ArrayList<String> bookPages = new ArrayList<>();
 
@@ -36,4 +42,23 @@ public class Events implements Listener {
         player.getInventory().addItem(book);
     }
 
+    @EventHandler
+    public void onSignChange(SignChangeEvent event) {
+        Block block = event.getBlock();
+        if(event.getLines()[0].equalsIgnoreCase(DevathonPlugin.INSTANCE.SignHead)){
+            if(block.getType() == Material.WALL_SIGN){
+                Sign s = (Sign)block.getState().getData();
+                Block attachedBlock = block.getRelative(s.getAttachedFace());
+
+                if(attachedBlock.getType() == DevathonPlugin.INSTANCE.MachineBlock){
+                    event.getPlayer().sendMessage("Successfully created a machine! " + block.getType().toString());
+                } else {
+                    event.getPlayer().sendMessage(attachedBlock.getType().name() + " is not a valid machine block! Please use " + DevathonPlugin.INSTANCE.MachineBlock.);
+                }
+            } else{
+                event.getPlayer().sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Machine sign must be placed on side of block! ");
+            }
+        }
+        System.out.println(event.getLines()[0]);
+    }
 }
